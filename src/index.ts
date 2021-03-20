996adabc519a724e84cce2973104f11b1c33ec73
Marcon Willian <oi@marconwillian.dev>
2021-03-20 18:16:41 -0300

    fix 🐛: Fix erros and add optional in second values

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,39 @@
 export interface OptionsRealToFloat {
-    decimal: number;
+  decimal: number;
 }
 
 export interface OptionsRealToInt {
-    useDecimal: boolean;
+  useDecimal: boolean;
 }
-  
-export const realToFloat = (real: string, {decimal}: OptionsRealToFloat): number => {
-    let number = real;
 
-    number = number.replace(/[^0-9,-]+/g,"");
-    number = number.replace(/,/g,".");
-    
-    return parseFloat(parseFloat(number).toFixed(decimal));
+export const realToFloat = (
+  real: string,
+  options?: OptionsRealToFloat
+): number => {
+  let number = real;
+
+  number = number.replace(/[^0-9,-]+/g, '');
+  number = number.replace(/,/g, '.');
+
+  return parseFloat(parseFloat(number).toFixed(options?.decimal || 2));
 };
 
-export const realToInt = (real: string, {useDecimal}: OptionsRealToInt): number => {
-    let number: string = real;
-    let integer: number;
+export const realToInt = (real: string, options?: OptionsRealToInt): number => {
+  let number: string = real;
+  let integer: number;
 
-    if(!useDecimal){
-        number = number.replace(/[^0-9,-]+/g,"");
-        number = number.replace(/,/g,".");
-        integer = parseFloat(number);
-        integer = Math.round(integer);
-        number = `${integer}`;
-    } else {
-        number = number.replace(/[^0-9,-]+/g,"");
-        number = number.replace(/,/g,".");
-        integer = parseFloat(parseFloat(number).toFixed(2));
-        number = `${integer}`.replace(/[^0-9-]+/g,"");
-    }
+  if (!options?.useDecimal) {
+    number = number.replace(/[^0-9,-]+/g, '');
+    number = number.replace(/,/g, '.');
+    integer = parseFloat(number);
+    integer = Math.round(integer);
+    number = `${integer}`;
+  } else {
+    number = number.replace(/[^0-9,-]+/g, '');
+    number = number.replace(/,/g, '.');
+    number = parseFloat(number).toFixed(2);
+    number = `${number}`.replace(/[^0-9-]+/g, '');
+  }
 
-    return parseInt(number);
+  return parseInt(number, 10);
 };
